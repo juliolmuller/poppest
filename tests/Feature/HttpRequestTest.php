57b6833a61abce962c57bd95e199e045658a5a79
Julio Muller <juliodolago@gmail.com>
2019-09-01 00:59:52 -0300

    Recreated methods to test routes and Http responses

--- tests/Feature/HttpRequestTest.php
+++ tests/Feature/HttpRequestTest.php
@@ -4,10 +4,22 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Ixudra\Curl\Facades\Curl;
+use App\Services\Tokenizer;
+use App\Language;
+use App\Repository;
 
 class HttpRequestTest extends TestCase
 {
     use DatabaseTransactions;
+
+    const BASE_API_URL = 'http://127.0.0.1:8000'; // Base API URL to submit HTTP requests
+    const API_URL = [                             // API URL's to test
+        self::BASE_API_URL . '/api/languages' => 'get',
+        self::BASE_API_URL . '/api/repositories/1' => 'get',
+        self::BASE_API_URL . '/api/load' => 'post', // <= this may exceed server processing timeout
+        self::BASE_API_URL . '/api/load/1' => 'post',
+    ];
 
     /**
      * Test if home page is responding properly.
@@ -18,26 +30,80 @@ class HttpRequestTest extends TestCase
     {
         $response = $this->get('/');
         $response->assertStatus(200);
-        $response->assertSee('Pick your favorite language');
+        $response->assertSee('Poppest');
+        $response->assertSee('<div id="root"></div>'); // Key tag to initialize React application
     }
 
     /**
-     * Check if database records are properly being retrieved.
+     * Check if API routes are properly refusing requests with HTTP status 403
      *
      * @test
      */
-    public function display_repository_api_test()
+    public function api_route_denied_status_test()
     {
-        $repositories = \App\Repository::all();
-        foreach ($repositories as $repository) {
-            $response = $this->json('GET', "/api/repository/{$repository->id}");
-            $response->assertStatus(200);
-            $response->assertJsonFragment([
-                'full_name' => "{$repository->owner}/{$repository->name}",
-                'url' => $repository->url,
-                'stars' => $repository->stars,
-                'forks' => $repository->forks
+        foreach (self::API_URL as $url => $method) {
+            $response = call_user_func([$this, $method], $url);
+            $response->assertStatus(403);
+        }
+    }
+
+    /**
+     * Check if API routes are properly accepting requests
+     *
+     * @test
+     */
+    public function api_route_accepted_test()
+    {
+        $token = new Tokenizer(time());
+        foreach (self::API_URL as $url => $method) {
+            $response = call_user_func([
+                Curl::to($url)->withHeaders([
+                    "X-TIMESTAMP: {$token->getTimestamp()}",
+                    "X-TOKEN: {$token->getToken()}",
+                ])->returnResponseObject(),
+                $method
             ]);
+            $this->assertEquals(200, $response->status);
+        }
+    }
+
+    /**
+     * Check if API routes are properly refusing requests and displaying the expected messages
+     *
+     * @test
+     */
+    public function api_route_denied_message_test()
+    {
+        // Validate no-timestamp/no-token requests
+        foreach (self::API_URL as $url => $method) {
+            $response = json_decode(call_user_func([Curl::to($url), $method]), true);
+            $this->assertEquals('Request does not contain the validation token', $response['error']);
+        }
+
+        // Validate expired requests
+        $token = new Tokenizer(1000);
+        foreach (self::API_URL as $url => $method) {
+            $response = json_decode(call_user_func([
+                Curl::to($url)->withHeaders([
+                    "X-TIMESTAMP: {$token->getTimestamp()}",
+                    "X-TOKEN: {$token->getToken()}",
+                ]),
+                $method
+            ]), true);
+            $this->assertEquals('Token has expired', $response['error']);
+        }
+
+        // Validate erroneous tokens
+        $token = new Tokenizer(time(), 'any-erroneous-token');
+        foreach (self::API_URL as $url => $method) {
+            $response = json_decode(call_user_func([
+                Curl::to($url)->withHeaders([
+                    "X-TIMESTAMP: {$token->getTimestamp()}",
+                    "X-TOKEN: {$token->getToken()}",
+                ]),
+                $method
+            ]), true);
+            $this->assertEquals('Token is invalid', $response['error']);
         }
     }
 
@@ -48,25 +114,26 @@ class HttpRequestTest extends TestCase
      */
     public function load_fresh_data_to_database_api_test()
     {
-        $response = $this->json('POST', '/api/load/');
-        $response->assertStatus(200);
-        $response->assertJson([
-            'success' => 'All languages updated successfully,'
-        ]);
-    }
+        // Validate the update of a single language
+        $lang = 1;
+        $token = new Tokenizer(time());
+        $response = json_decode(Curl::to(self::BASE_API_URL . "/api/load/{$lang}")
+            ->withHeaders([
+                "X-TIMESTAMP: {$token->getTimestamp()}",
+                "X-TOKEN: {$token->getToken()}",
+            ])
+            ->post(),
+        true);
+        $this->assertEquals("Languages #{$lang} updated successfully", $response['success']);
 
-    /**
-     * Check if database records are retrievable.
-     *
-     * @test
-     */
-    public function show_repositories_api_test()
-    {
-        $languages = \App\Language::all();
-        foreach ($languages as $language) {
-            $response = $this->post("/api/show/{$language->id}");
-            $response->assertStatus(200);
-            $response->assertSee("\"panel-content-{$language->id}\"");
-        }
+        // Validate the update of all languages
+        $response = json_decode(Curl::to(self::BASE_API_URL . '/api/load')
+            ->withHeaders([
+                "X-TIMESTAMP: {$token->getTimestamp()}",
+                "X-TOKEN: {$token->getToken()}",
+            ])
+            ->post(),
+        true);
+        $this->assertEquals('All languages updated successfully', $response['success']);
     }
 }
