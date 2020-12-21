33ed6ceb08d2c5cb5be89efb139762d0967c3c9f
Julio Muller <juliodolago@gmail.com>
2020-12-21 15:20:10 -0300

    Removed commentaries

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,6 @@
+/* eslint-disable */
 const mix = require('laravel-mix')
-require('laravel-mix-react-typescript-extension');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.disableNotifications()
+require('laravel-mix-react-typescript-extension')
 
 mix
   .reactTypeScript('resources/scripts/index.tsx', 'public/js')
